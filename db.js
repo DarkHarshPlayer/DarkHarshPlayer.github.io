b6e457e1139ab2bf97210163dd4cd534fecad48e
Harsh <34979806+DarkHarshPlayer@users.noreply.github.com>
2018-06-24 19:43:47 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,349 @@
+let songsDB =[{
+    'name':'Perfect',
+    'src':'05 - Perfect.mp3',
+    'artist':'Ed Sheeran',
+    'album':'Divide Album Cover.jpg'
+},
+
+{
+    'name':'One call away',
+    'src':'https://dl.dropbox.com/s/g2wdelflcuaumjs/1-01%20One%20Call%20Away.mp3',
+    'artist':'Charlie Puth',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Charlie_Puth_-_Nine_Track_Mind.png/220px-Charlie_Puth_-_Nine_Track_Mind.png'
+},
+
+{
+    'name':'We don&lsquo;t talk anymore',
+    'src':'https://dl.dropbox.com/s/3qojbr3t2h0h6eb/1-05%20We%20Don%27t%20Talk%20Anymore%20%28feat.%20Se.mp3',
+    'artist':'Charlie Puth ft.Selena',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Charlie_Puth_Feat._Selena_Gomez_-_We_Don%27t_Talk_Anymore_%28Official_Single_Cover%29.png/220px-Charlie_Puth_Feat._Selena_Gomez_-_We_Don%27t_Talk_Anymore_%28Official_Single_Cover%29.png'
+},
+
+{
+    'name':'See you again(solo)',
+    'src':'https://dl.dropbox.com/s/guwmk2hmyx0rdes/1-13%20See%20You%20Again%20%28Solo%20Version%29.mp3',
+    'artist':'Charlie Puth',
+    'album':'https://i.ytimg.com/vi/3pMdQAO7iuk/maxresdefault.jpg'
+},
+
+{
+    'name':'Boyfriend',
+    'src':'https://dl.dropbox.com/s/9i5arlyjw8m732m/02.%20Boyfriend.mp3',
+    'artist':'Justin Bieber',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/2/20/Justin_Bieber_-_Boyfriend.jpg/220px-Justin_Bieber_-_Boyfriend.jpg'
+},
+
+{
+    'name':'Drag me down',
+    'src':'https://dl.dropbox.com/s/bq25xe7ds05demp/02.%20Drag%20Me%20Down.mp3',
+    'artist':'One Direction',
+    'album':'https://is2-ssl.mzstatic.com/image/thumb/Music18/v4/df/7d/d3/df7dd316-a3e2-01d1-6698-dfbd5e451dbb/mzm.oxshyntj.jpg/1200x630bb.jpg'
+},
+
+{
+    'name':'Despacito',
+    'src':'http://mp3.flowhot.net/mp3/wp-content/uploads/2017/03/Luis%20Fonsi%20-%20Despacito%20%28Versi%C3%B3n%20Pop%29.mp3',
+    'artist':'Luis Fonsi ft.Daddy yankee',
+    'album':'https://upload.wikimedia.org/wikipedia/en/c/c8/Luis_Fonsi_Feat._Daddy_Yankee_-_Despacito_%28Official_Single_Cover%29.png'
+},
+
+{
+    'name':'What do you mean',
+    'src':'https://dl.dropbox.com/s/hl0pxno1o6h8xxp/03%20-%20What%20Do%20You%20Mean.mp3',
+    'artist':'Justin Bieber',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/JustinBieberWhatDoYouMeanCover.png/220px-JustinBieberWhatDoYouMeanCover.png'
+},
+
+{
+    'name':'As long as you love me',
+    'src':'https://dl.dropbox.com/s/76uilsys64drqi4/03.%20As%20Long%20As%20You%20Love%20Me.mp3',
+    'artist':'Justin Bieber ft. Big Sean',
+    'album':'https://images.genius.com/b1f93059d46a37a8931055ecfed27834.1000x1000x1.jpg'
+},
+
+{
+    'name':'Perfect',
+    'src':'https://dl.dropbox.com/s/1xucnvujlpflx5n/03.%20Perfect.mp3',
+    'artist':'One Direction',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/One_Direction_-_Perfect.png/220px-One_Direction_-_Perfect.png'
+},
+
+{
+    'name':'History',
+    'src':'https://dl.dropbox.com/s/cb7ln3z97ezpos2/13.%20History.mp3',
+    'artist':'One Direction',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/One_Direction_-_Made_in_the_AM_%28Official_Album_Cover%29.png/220px-One_Direction_-_Made_in_the_AM_%28Official_Album_Cover%29.png'
+},
+
+{
+    'name':'Baby',
+    'src':'https://dl.dropbox.com/s/6hbvgqbyud11i4a/209-justin_bieber-baby_feat._ludacris.mp3',
+    'artist':'Justin Bieber ft. Ludacris',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Babycoverart.jpg/220px-Babycoverart.jpg'
+},
+
+{
+    'name':'Faded',
+    'src':'https://dl.dropbox.com/s/50w5jamtil3hg3a/Faded%20%28Alan%20Walker%29%20.mp3?',
+    'artist':'Alan Walker',
+    'album':'https://2.bp.blogspot.com/-zEGSruGyqNA/WGS2SLBupwI/AAAAAAAAApU/aqo89sdE2d4U9rerY3SMIbAu97fQEAgVQCLcB/s1600/alam-walker-faded-song-lyrics-750750.jpg'
+},
+
+{
+    'name':'Shape of you',
+    'src':'https://dl.dropbox.com/s/15z6oujjpnk4j6e/04%20-%20Shape%20Of%20You.mp3',
+    'artist':'Ed Sheeran',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Shape_Of_You_%28Official_Single_Cover%29_by_Ed_Sheeran.png/220px-Shape_Of_You_%28Official_Single_Cover%29_by_Ed_Sheeran.png'
+},
+
+{
+    'name':'Sorry',
+    'src':'https://dl.dropbox.com/s/p0kgd3f9l9e8ris/04%20-%20Sorry.mp3',
+    'artist':'Justin Bieber',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/Justin_Bieber_-_Sorry_%28Official_Single_Cover%29.png/220px-Justin_Bieber_-_Sorry_%28Official_Single_Cover%29.png'
+},
+
+{
+    'name':'Love yourself',
+    'src':'https://dl.dropbox.com/s/qmwv3nuy3nhk9m0/05%20-%20Love%20Yourself.mp3',
+    'artist':'Justin Bieber',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/JustinBieberLoveYourself.png/220px-JustinBieberLoveYourself.png'
+},
+
+{
+    'name':'Company',
+    'src':'https://dl.dropbox.com/s/kou4wk7susfare5/06%20-%20Company.mp3',
+    'artist':'Justin Bieber',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/0/0b/Company_remix_cover.png/220px-Company_remix_cover.png'
+},
+
+{
+    'name':'Computer Boy',
+    'src':'https://dl.dropbox.com/s/86mick0vrtg0ehl/6%20-%20Poppy%20-%20Computer%20Boy.mp3',
+    'artist':'Poppy',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Poppy_-_Poppy_Computer.png/220px-Poppy_-_Poppy_Computer.png'
+},
+
+{
+    'name':'Interweb',
+    'src':'https://dl.dropbox.com/s/xnqkefe5zgbu8tu/9%20-%20Poppy%20-%20Interweb.mp3',
+    'artist':'Poppy',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Poppy_-_Poppy_Computer.png/220px-Poppy_-_Poppy_Computer.png'
+},
+
+{
+    'name':'Software upgrade',
+    'src':'https://dl.dropbox.com/s/5ng4pv4mf173bit/10%20-%20Poppy%20-%20Software%20Upgrade.mp3',
+    'artist':'Poppy',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Poppy_-_Poppy_Computer.png/220px-Poppy_-_Poppy_Computer.png'
+},
+
+{
+    'name':'Beauty and the beat',
+    'src':'https://dl.dropbox.com/s/j8xg0t1utl919wb/10.%20Beauty%20And%20A%20Beat.mp3',
+    'artist':'Justin Bieber ft. Nicki Minaj',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/3/31/Justin_Bieber_-_Beauty_and_a_Beat.png/220px-Justin_Bieber_-_Beauty_and_a_Beat.png'
+},
+
+{
+    'name':'Eenie Meenie',
+    'src':'https://dl.dropbox.com/s/k580834u3ybm0ra/216-justin_bieber-eenie_meenie_feat._sean_kingston.mp3',
+    'artist':'Justin Bieber ft.Sean Kingston',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Eeniemeeniecover.jpg/220px-Eeniemeeniecover.jpg'
+},
+
+{
+    'name':'One less Lonely Girl',
+    'src':'https://dl.dropbox.com/s/0rwc6iyx5js1fxl/205-justin_bieber-one_less_lonely_girl.mp3',
+    'artist':'Justin Bieber',
+    'album':'https://i.scdn.co/image/eb84299943e4918fca4058790ccfc5f6da150521'
+},
+
+{
+    'name':'Stuck In the moment',
+    'src':'https://dl.dropbox.com/s/yg6joocw0aw13ek/211-justin_bieber-stuck_in_the_moment.mp3',
+    'artist':'Justin Bieber',
+    'album':'https://i.scdn.co/image/eb84299943e4918fca4058790ccfc5f6da150521'
+},
+
+{
+    'name':'Stitches',
+    'src':'https://dl.dropbox.com/s/bmu153czlljpqvx/02%20Stitches.mp3',
+    'artist':'Shawn Mendes',
+    'album':'https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.rapgenius.com%2F3b4e501efa5af6642e9f15e1452b8026.600x600x1.jpg'
+},
+
+{
+    'name':'Treat You Better',
+    'src':'https://dl.dropbox.com/s/2p7ghq8osqufxru/3.%20Treat%20You%20Better.mp3',
+    'artist':'Shawn Mendes',
+    'album':'https://img-fanburst.freetls.fastly.net/x44vEffAGHZ1B2IuCuu9f0y_7C0=/400x400/cx2.fanburst.com/artwork/4ea2132b-62e9-463a-bb7f-5db98fc302c1.JPG'
+},
+
+{
+    'name':'Mercy',
+    'src':'https://dl.dropbox.com/s/2t6r2mc31anrsha/2.%20Mercy.mp3',
+    'artist':'Shawn Mendes',
+    'album':'https://images.genius.com/ced6fec4819853ecb50da2322f9d46a9.1000x1000x1.jpg'
+},
+
+{
+    'name':'Gallway Girl',
+    'src':'https://dl.dropbox.com/s/q4zfm3fho5jg6rl/06%20-%20Galway%20Girl.mp3',
+    'artist':'Ed Sheeran',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/9/95/EdSheeranGG.jpg/220px-EdSheeranGG.jpg'
+},
+
+{
+    'name':'Happier',
+    'src':'https://dl.dropbox.com/s/8wj3oozehtyuzqn/07%20-%20Happier.mp3',
+    'artist':'Ed Sheeran',
+    'album':'https://images.genius.com/40d09c0b867729a2bfe0a01afd82991c.720x720x1.jpg'
+},
+
+{
+    'name':'Castle On The Hills',
+    'src':'https://dl.dropbox.com/s/lntg8flhcf9gdpo/02%20-%20Castle%20On%20The%20Hill.mp3',
+    'artist':'Ed Sheeran',
+    'album':'http://www.wdjx.com/wp-content/uploads/2017/01/Ed-Sheeran-Castle-On-The-Hill-Official-Lyric-Video.jpeg'
+},
+
+{
+    'name':'Steal My Girl',
+    'src':'https://dl.dropbox.com/s/k1ywpayk3w807b1/01%20Steal%20My%20Girl.mp3',
+    'artist':'One Direction',
+    'album':'https://vignette.wikia.nocookie.net/onedirection/images/c/c2/Steal_My_Girl_cover.png/revision/latest?cb=20150222024431'
+},
+
+{
+    'name':'Ready To Run',
+    'src':'https://dl.dropbox.com/s/23s4aw7v4ajib02/02%20Ready%20to%20Run.mp3',
+    'artist':'One Direction',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/One_Direction_-_Four.png/220px-One_Direction_-_Four.png'
+},
+
+{
+    'name':'Night Changes',
+    'src':'https://dl.dropbox.com/s/yosi5w80jd60vj5/07%20Night%20Changes.mp3',
+    'artist':'One Direction',
+    'album':'http://4.bp.blogspot.com/-57Z5u0MxJpM/VREeAo0irdI/AAAAAAAAAUE/Hj26DsvebAo/s1600/tumblr_nf8wsfMGIl1rlrb97o1_500.png'
+},
+
+{
+    'name':'Story Of My Life',
+    'src':'https://dl.dropbox.com/s/0884vv40mv80qag/1-02%20Story%20of%20My%20Life.mp3',
+    'artist':'One Direction',
+    'album':'https://2.bp.blogspot.com/-5FDt71zXGJM/Uqg4ETmMvmI/AAAAAAAACMs/2KYck-oQDt8/s1600/One-Direction-Midnight-Memories-Deluxe-CD-Album.jpg'
+},
+
+{
+    'name':'Strong',
+    'src':'https://dl.dropbox.com/s/sm2b9f6wjk2yt65/1-07%20Strong.mp3',
+    'artist':'One Direction',
+    'album':'https://2.bp.blogspot.com/-5FDt71zXGJM/Uqg4ETmMvmI/AAAAAAAACMs/2KYck-oQDt8/s1600/One-Direction-Midnight-Memories-Deluxe-CD-Album.jpg'
+},
+
+{
+    'name':'Best Song Ever',
+    'src':'https://dl.dropbox.com/s/h7b5cc88rg24z7m/1-01%20Best%20Song%20Ever.mp3',
+    'artist':'One Direction',
+    'album':'https://c.saavncdn.com/445/Best-Song-Ever-2013-500x500.jpg'
+},
+
+{
+    'name':'I Want',
+    'src':'https://dl.dropbox.com/s/q0nt647lebsw4z0/09-one_direction-i_want.mp3',
+    'artist':'One Direction',
+    'album':'https://upload.wikimedia.org/wikipedia/en/9/96/One_direction_up_all_night_albumcover.jpg'
+},
+
+{
+    'name':'What Makes You Beautiful',
+    'src':'https://dl.dropbox.com/s/c4926e4bdeqi1oi/01-one_direction-what_makes_you_beautiful.mp3',
+    'artist':'One Direction',
+    'album':'https://upload.wikimedia.org/wikipedia/en/9/96/One_direction_up_all_night_albumcover.jpg'
+},
+
+{
+    'name':'One Thing',
+    'src':'https://dl.dropbox.com/s/mk02ecyf1hdrixa/03-one_direction-one_thing.mp3',
+    'artist':'One Direction',
+    'album':'https://upload.wikimedia.org/wikipedia/en/9/96/One_direction_up_all_night_albumcover.jpg'
+},
+
+{
+    'name':'Up all Night',
+    'src':'https://dl.dropbox.com/s/lepbp1l1o6ho2sz/05-one_direction-up_all_night.mp3',
+    'artist':'One Direction',
+    'album':'https://upload.wikimedia.org/wikipedia/en/9/96/One_direction_up_all_night_albumcover.jpg'
+},
+
+{
+    'name':'Mockingbird',
+    'src':'https://dl.dropbox.com/s/cu5sd29m6g0w1h8/17%20-%20Mockingbird.mp3',
+    'artist':'Eminem',
+    'album':'https://i.scdn.co/image/4a8484bc28e3c1432db5d77728391e665af8b296'
+},
+
+{
+    'name':'Animals',
+    'src':'https://dl.dropbox.com/s/dc4yq3js50tojvf/Maroon%205%20-%20Animals.mp3',
+    'artist':'Maroon 5',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Maroon_5_-_V_%28Official_Album_Cover%29.png/220px-Maroon_5_-_V_%28Official_Album_Cover%29.png'
+},
+
+{
+    'name':'Sugar',
+    'src':'https://dl.dropbox.com/s/k483q8wbb1w7mfm/Sugar.mp3',
+    'artist':'Maroon 5',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Maroon_5_-_V_%28Official_Album_Cover%29.png/220px-Maroon_5_-_V_%28Official_Album_Cover%29.png'
+},
+
+{
+    'name':'How Long',
+    'src':'https://dl.dropbox.com/s/9qtn74q37bm11oz/04.%20How%20Long.mp3',
+    'artist':'Charlie Puth',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Charlie_Puth_Voicenotes.png/220px-Charlie_Puth_Voicenotes.png'
+},
+
+{
+    'name':'Attention',
+    'src':'https://dl.dropbox.com/s/kaw0xhp0e5o4tba/02.%20Attention.mp3',
+    'artist':'Charlie Puth',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Charlie_Puth_Voicenotes.png/220px-Charlie_Puth_Voicenotes.png'
+},
+
+{
+    'name':'Melody',
+    'src':'https://dl.dropbox.com/s/eoljxhbpa0e4l8r/01%20-%20Melody%20%28feat.%20Jon%20Becker%29.mp3',
+    'artist':'Cadmium ft. Jon Becker',
+    'album':'https://is4-ssl.mzstatic.com/image/thumb/Music128/v4/28/50/32/28503286-c802-7b58-d6c7-95ae9a1a56c0/cover.jpg/268x0w.jpg'
+},
+
+{
+    'name':'Its Everyday Bro',
+    'src':'https://dl.dropbox.com/s/zh8v4uxf281lr2e/its%20everyday%20bro.mp3',
+    'artist':'Jack Paul ft. Team 10',
+    'album':'https://images.genius.com/40b2f28c212dd5408e03e5a6e216e833.1000x1000x1.jpg'
+},
+
+{
+    'name':'Alone',
+    'src':'https://dl.dropbox.com/s/5r6hwe9lcl9f53g/Alone%20-%20%5BAlan%20Walker%5D.mp3',
+    'artist':'Alan Walker',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Alan_Walker_-_Alone.png/220px-Alan_Walker_-_Alone.png'
+},
+
+{
+    'name':'Havana',
+    'src':'https://dl.dropbox.com/s/51eaf2nlv4bmq6d/01%20Havana%20%28feat.%20Young%20Thug%29.mp3',
+    'artist':'Camilla Cabello ft. Young Thug',
+    'album':'https://upload.wikimedia.org/wikipedia/en/thumb/9/98/Havana_%28featuring_Young_Thug%29_%28Official_Single_Cover%29_by_Camila_Cabello.png/220px-Havana_%28featuring_Young_Thug%29_%28Official_Single_Cover%29_by_Camila_Cabello.png'
+},
+
+{
+    'name':'On &amp; On',
+    'src':'https://dl.dropbox.com/s/6h2l7djfrur3dg0/Cartoon%20-%20On%20_%20On%20%28feat.%20Daniel%20Levi%29%20_NCS%20Release_.mp3',
+    'artist':'Cartoon',
+    'album':'https://is5-ssl.mzstatic.com/image/thumb/Music128/v4/fc/a6/63/fca663d5-11eb-a0fd-4090-66adc426fc5a/cover.jpg/600x600bf.jpg'
+}];
